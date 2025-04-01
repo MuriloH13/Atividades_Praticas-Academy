e47a6a667ecafc2db501672e51ac765fd3002296
Murilo <murilo.b08@aluno.ifsc.edu.br>
2025-04-01 08:45:07 -0300

    Commit Ap2-Coleções

---
+++ ap2_colecoes/ap2.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:core';
+
+void main() {
+  final random = Random();
+
+  List<int> lista = List.generate(50, (_) => random.nextInt(15));
+
+  stdout.write('Lista Original: ');
+  for (var indice = 0; indice < lista.length; indice++) {
+    stdout.write('${lista[indice]} ; ');
+  }
+
+  for (var indice = 0; indice < lista.length; indice++) {
+    lista.removeWhere((item) => item.isEven);
+  }
+  stdout.write('\nLista Atualizada: ');
+  for (var indice = 0; indice < lista.length; indice++) {
+    stdout.write('${lista[indice]} ; ');
+  }
+}
