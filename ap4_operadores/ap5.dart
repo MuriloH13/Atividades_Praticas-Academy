3e8fc33cf673811d54185864d1f0cf15ef542e1f
Murilo <murilo.b08@aluno.ifsc.edu.br>
2025-04-04 10:59:35 -0300

    Commit Ap5 - Operadores

---
+++ ap4_operadores/ap5.dart
@@ -0,0 +1,16 @@
+void main() {
+  Map<String, int?> pessoas = {
+    'Nelson': null,
+    'Jane': null,
+    'Jack': 16,
+    'Rupert': 37,
+    'Andy': 13,
+    'Kim': 27,
+    'Robert': 31
+  };
+
+  for (final nome in pessoas.keys) {
+    final idade = pessoas[nome];
+    print('$nome - ${idade ?? 'idade não informada'}');
+  }
+}
