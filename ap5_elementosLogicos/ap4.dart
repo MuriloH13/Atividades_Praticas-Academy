778890d4c4fb1008f301c3d32385c48e8c09463e
Murilo <murilo.b08@aluno.ifsc.edu.br>
2025-04-07 14:57:15 -0300

    Commit Ap4 - Elementos lógicos

---
+++ ap5_elementosLogicos/ap4.dart
@@ -0,0 +1,37 @@
+void main() {
+  final listaNomes = [
+    "Joao",
+    "Maria",
+    "Pedro",
+    "Maria",
+    "Ana",
+    "Joao",
+    "Maria",
+    "Fernanda",
+    "Carlos",
+    "Maria"
+  ];
+
+  final nome = 'Ana';
+  final quantidade = contarNome(listaNomes, nome);
+
+  if (quantidade == 1) {
+    print('O nome $nome foi encontrado 1 vez');
+  } else if (quantidade > 0) {
+    print('O nome $nome foi encontrado $quantidade vezes');
+  } else {
+    print('O nome nao foi encontrado');
+  }
+}
+
+int contarNome(List<String> listaNomes, String nome) {
+  int quantidade = 0;
+
+  for (final nomeRepetido in listaNomes) {
+    if (nome == nomeRepetido) {
+      quantidade++;
+    }
+  }
+
+  return quantidade;
+}
