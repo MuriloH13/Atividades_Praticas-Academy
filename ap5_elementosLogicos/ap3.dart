334109ca06e72e69dc245f78d29b00fea2bc5e53
Murilo <murilo.b08@aluno.ifsc.edu.br>
2025-04-07 14:44:31 -0300

    Commit Ap3 - Elementos lógicos

---
+++ ap5_elementosLogicos/ap3.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  final random = Random();
+
+  final numeroAleatorio = random.nextInt(900) + 100;
+
+  final somaAleatorio = somaPares(numeroAleatorio);
+  print('A soma dos números pares entre 0 e $numeroAleatorio e $somaAleatorio');
+}
+
+int somaPares(int numero) {
+  var indice = 0;
+  var soma = 0;
+  while (indice <= numero) {
+    if (indice.isEven) {
+      soma += indice;
+    }
+    indice++;
+  }
+
+  return soma;
+}
